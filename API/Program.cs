7b7045314e4a11ac1f7874e5169a2e1f6e5a16da
Jewel Nath <jewel.nath@orbitax.com>
2024-05-14 13:42:04 +0600

    End of section 7 (Error Handling )

--- API/Program.cs
+++ API/Program.cs
@@ -1,32 +1,28 @@
-using API.Data;
 using API.Extensions;
-using API.Interfaces;
-using API.Services;
-using Microsoft.AspNetCore.Authentication.JwtBearer;
-using Microsoft.EntityFrameworkCore;
-using Microsoft.IdentityModel.Tokens;
-using System.Text;
-
+using API.Middleware;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
 builder.Services.AddControllers();
 builder.Services.AddApplicationServices(builder.Configuration);
 builder.Services.AddIdentitySerivices(builder.Configuration);
 
 var app = builder.Build();
 
+if(builder.Environment.IsDevelopment())
+{
+    app.UseDeveloperExceptionPage();
+}
+
 // Configure the HTTP request pipeline.
+app.UseMiddleware<ExceptionMiddleware>();
 app.UseCors(
     builder => builder.AllowAnyHeader().AllowAnyMethod()
         .WithOrigins("https://localhost:4200")
 );
-
 app.UseAuthentication();
 app.UseAuthorization();
-
 app.MapControllers();
 
 app.Run();
