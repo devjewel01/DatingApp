97d4fba9da3431afc808d34b21678a84bb472e8d
Jewel Nath <jewel.nath@orbitax.com>
2024-05-15 17:33:02 +0600

    End of section 9 ( Jwt Interceptor, Photo Gallery)

--- client/src/app/_services/account.service.ts
+++ client/src/app/_services/account.service.ts
@@ -2,6 +2,7 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { BehaviorSubject, map } from 'rxjs';
 import { User } from '../_models/user';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root'
@@ -9,7 +10,7 @@ import { User } from '../_models/user';
 
 export class AccountService {
 
-  baseUrl = "https://localhost:5001/api/";
+  baseUrl = 'https://localhost:5001/api/';
   private currentUserSource = new BehaviorSubject<User | null>(null);
   currentUser$ = this.currentUserSource.asObservable();
 
--- client/src/app/home/home.component.ts
+++ client/src/app/home/home.component.ts
@@ -1,5 +1,5 @@
-import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+
 
 @Component({
   selector: 'app-home',
@@ -10,10 +10,10 @@ export class HomeComponent implements OnInit{
   registerMode = false;
   users: any = {};
 
-  constructor(private http:HttpClient) {}
+  constructor() {}
 
   ngOnInit(): void{
-    this.getUsers();
+
   }
 
   registerToggle()
@@ -21,20 +21,7 @@ export class HomeComponent implements OnInit{
       this.registerMode = !this.registerMode;
   }
 
-  getUsers()
-  {
-    this.http.get('https://localhost:5001/api/users').subscribe({
-      next: response => {
-        this.users = response;
-      },
-      error: error => {
-        console.error('There was an error!', error);
-      },
-      complete : () => {
-        console.log('Request Completed');
-      }
-    })
-  }
+
 
   cancelRegistrationMode(event: boolean)
   {
